a42c6c7d5566170f78dbe77d11faebb8e15ec276
bahadir <baha_i@hotmail.com>
2022-02-28 14:43:40 +0100

    enter conflict gelöst

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route} from "react-router-dom";
 import Login from './components/Login';
 import Search from './components/Search';
 
 export default function App() {
   return (
   <Routes>
-    <Route path="/" element={<Login />}/>
+    <Route path="/" element={<Login />}>
+    
+
+    </Route>
     <Route path="/search" element={<Search />} />
    
   </Routes>
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,39 +1,30 @@
-import React, {useState} from "react";
-import {Link} from "react-router-dom";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 import { Form, Button } from "react-bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "../css/login.css";
 
 const Login = () => {
+  const [email, setEmail] = useState("");
 
-const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
-const [password, setPassword] = useState("");
+  function validateForm() {
+    return email.length > 9 && password.length > 6;
+  }
 
-function validateForm() {
+  function handleSubmit(e) {
+    e.preventDefault();
+    localStorage.setItem("password", password);
+    localStorage.setItem("email", email);
+    // setPassword("");
+    // setEmail("");
+   
+  }
 
-  return email.length > 9  && password.length > 6;
-
-}
-
-
-function handleSubmit(e) {
-
-  e.preventDefault();
-  localStorage.setItem("password", password);
-  localStorage.setItem("email", email);
-  setPassword("");
-  setEmail("");
-  
- 
-
-}
-
-return (
-
-  <div className="Login">
-
-    <Form onSubmit={handleSubmit}>
+  return (
+    <div className="Login">
+      {/* <Form onSubmit={handleSubmit}>
 
       <Form.Group size="lg" controlId="email">
 
@@ -75,13 +66,46 @@ return (
 
       </Button>
 
-    </Form>
+    </Form> */}
 
-  </div>
+      <Form onSubmit={handleSubmit}>
+        
+        <Form.Group className="mb-3" controlId="formBasicEmail">
+          <Form.Label>Email address</Form.Label>
+          <Form.Control
+            autoFocus
+            type="email"
+            placeholder="Enter email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <Form.Text className="text-muted">
+            We'll never share your email with anyone else.
+          </Form.Text>
+        </Form.Group>
 
-);
-
-
+        <Form.Group className="mb-3" controlId="formBasicPassword">
+          <Form.Label>Password</Form.Label>
+          <Form.Control
+            type="password"
+            placeholder="Password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </Form.Group>
+        {/* <Form.Group className="mb-3" controlId="formBasicCheckbox">
+          <Form.Check type="checkbox" label="Check me out" />
+        </Form.Group> */}
+        
+        <Link to="/search"><Button
+          variant="primary"
+          type="submit"
+          className="login-btn"
+          disabled={!validateForm()}
+        >Login</Button></Link>
+      </Form>
+    </div>
+  );
 };
 
 export default Login;
