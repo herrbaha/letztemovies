851a83396064640419cedd7a7356488c590e58e3
bahadir <baha_i@hotmail.com>
2022-03-06 22:04:37 +0100

    neue route erstellt

--- src/App.js
+++ src/App.js
@@ -1,12 +1,40 @@
-import { Routes, Route } from "react-router-dom";
-import Login from "./components/Login";
-import Search from "./components/Search";
+// import { Routes, Route } from "react-router-dom";
+// import Login from "./components/Login";
+// import Search from "./components/Search";
 
-export default function App() {
+// export default function App() {
+//   return (
+//     <Routes>
+//       <Route path="/" element={<Login />}></Route>
+//       <Route path="/search" element={<Search />} />
+//     </Routes>
+//   );
+// }
+import {
+  Route,
+  Redirect
+} from 'react-router-dom';
+
+function App({ search, isAuthenticated, ...rest }) {
   return (
-    <Routes>
-      <Route path="/" element={<Login />}></Route>
-      <Route path="/search" element={<Search />} />
-    </Routes>
+    <Route
+      {...rest}
+      render={
+        ({ location }) => (
+          isAuthenticated
+            ? (
+              search
+            ) : (
+              <Redirect
+                to={{
+                  pathname: '/login',
+                  state: { from: location }
+                }}
+              />
+            ))
+      }
+    />
   );
 }
+
+export default App;
