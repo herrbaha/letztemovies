2f69eec29a0be090936b0db839148678d8e35041
bahadir <baha_i@hotmail.com>
2022-03-07 19:55:09 +0100

    letzte router

--- src/App.js
+++ src/App.js
@@ -1,42 +1,42 @@
-// import { Routes, Route } from "react-router-dom";
-// import Login from "./components/Login";
-// import Search from "./components/Search";
-
-// export default function App() {
-//   return (
-//     <Routes>
-//       <Route path="/" element={<Login />}></Route>
-//       <Route path="/search" element={<Search />} />
-//     </Routes>
-//   );
-// }
-import {
-  Route,
-  Redirect, BrowserRouter, useAuth
-} from 'react-router-dom';
+import { Routes, Route } from "react-router-dom";
 import Login from "./components/Login";
 import Search from "./components/Search";
 
-function PrivateRoute({ children, ...rest }) {
-  let auth = useAuth();
+export default function App() {
   return (
-    <Route
-      {...rest}
-      render={() => auth
-        ? children
-        : <Redirect to="/login" />
-      }
-    />
-  );
-}
-
-function App() {
-  return (
-    <BrowserRouter>
+    <Routes>
       <Route path="/" element={<Login />}></Route>
-      <PrivateRoute path="/search" element={<Search />} />
-    </BrowserRouter>
+      <Route path="/search" element={<Search />} />
+    </Routes>
   );
 }
+// import {
+//   Route,
+//   Redirect, BrowserRouter, useAuth
+// } from 'react-router-dom';
+// import Login from "./components/Login";
+// import Search from "./components/Search";
 
-export default App;
+// function PrivateRoute({ children, ...rest }) {
+//   let auth = useAuth();
+//   return (
+//     <Route
+//       {...rest}
+//       render={() => auth
+//         ? children
+//         : <Redirect to="/login" />
+//       }
+//     />
+//   );
+// }
+
+// function App() {
+//   return (
+//     <BrowserRouter>
+//       <Route path="/" element={<Login />}></Route>
+//       <PrivateRoute path="/search" element={<Search />} />
+//     </BrowserRouter>
+//   );
+// }
+
+// export default App;
