04531669f3ac44b500492282d71c44e422aa94e3
bahadir <baha_i@hotmail.com>
2022-03-08 22:03:49 +0100

    auth problem, ich kann alles im local storage reservieren aber

--- src/App.js
+++ src/App.js
@@ -1,29 +1,30 @@
-import { Routes, Route, Navigate } from "react-router-dom";
+import { Routes, Route} from "react-router-dom";
 import Login from "./components/Login";
 import Search from "./components/Search";
 
 export default function App() {
 
-  const PrivateRoute = ({ children}) => {
-    const isAuthenticated = true;
+  // const PrivateRoute = ({ children}) => {
+  //   const isAuthenticated = true;
         
-    if (isAuthenticated ) {
-      return children
-    }
+  //   if (isAuthenticated ) {
+  //     return children
+  //   }
       
-    return <Navigate to="/" />
-  }
+  //   return <Navigate to="/" />
+  // }
 
   return (
     <Routes>
-      <Route
+      {/* <Route
           path="/"
           element={
             <PrivateRoute>
               <Login />
             </PrivateRoute>
-          }
-        />
+          }/> */}
+      
+          <Route path="/" element={<Login />} />
       <Route path="/search" element={<Search />} />
     </Routes>
   );
